89e61d782a6ecf707e8bd9bdcd28d136e4435814
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-04-15 22:01:11 +0200

    finilizing main page and start intent

--- neuronNetwork/app/src/main/java/world/waac/neuron/MainActivity.java
+++ neuronNetwork/app/src/main/java/world/waac/neuron/MainActivity.java
@@ -3,12 +3,26 @@ package world.waac.neuron;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //initial Button
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
