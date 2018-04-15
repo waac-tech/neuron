3c6bced096ec821cf1508c4228c6f3454fce816c
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-04-16 00:03:12 +0200

    frawable fixing

---
+++ neuronNetwork/app/src/main/java/world/waac/neuron/FragmentOne.java
@@ -0,0 +1,22 @@
+package world.waac.neuron;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentOne extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.id.fragment_one, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ neuronNetwork/app/src/main/java/world/waac/neuron/FragmentTwo.java
@@ -0,0 +1,25 @@
+package world.waac.neuron;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+
+    @SuppressLint("ResourceType")
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.id.fragment_two, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
