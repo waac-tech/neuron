75f70c7b1a82a54d830170806c3fb5fae4259aed
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-04-20 19:16:23 +0200

    new way

--- neuronNetwork/app/src/main/java/world/waac/neuron/SecondActivity.java
+++ neuronNetwork/app/src/main/java/world/waac/neuron/SecondActivity.java
@@ -1,54 +1,22 @@
 package world.waac.neuron;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
 
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-            //Fragment Manager
-            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-            android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    transaction.replace(R.id.content, new HomeFragment()).commit();
-                    return true;
-                case R.id.navigation_settings:
-                    transaction.replace(R.id.content, new SettingsFragment()).commit();
-                    return true;
-                case R.id.navigation_notifications:
-                    transaction.replace(R.id.content, new NotificationFragment()).commit();
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-        android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.content, new HomeFragment()).commit();
-
+        
+        initialWork();
     }
 
+    private void initialWork() {
+    }
 }
