94813fb92c1757612fffedbf28e2e8200602d39d
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-04-23 10:25:21 +0200

    btn method

--- neuronNetwork/app/src/main/java/world/waac/neuron/SecondActivity.java
+++ neuronNetwork/app/src/main/java/world/waac/neuron/SecondActivity.java
@@ -24,6 +24,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -51,6 +53,11 @@ public class SecondActivity extends AppCompatActivity {
     WifiP2pDevice[] deviceArray;
 
     static final int MESSAGE_READ = 1;
+
+
+    ServerClass serverClass;
+    ClientClass clientClass;
+    SendReceive sendReceive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,6 +143,13 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
     private void initialWork() {
@@ -218,10 +232,14 @@ public class SecondActivity extends AppCompatActivity {
                 if (wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner){
 
                     connectiomStatus.setText("HOST");
+                    serverClass = new ServerClass();
+                    serverClass.start();
 
                 } else if (wifiP2pInfo.groupFormed){
 
                     connectiomStatus.setText("CLIENT");
+                    clientClass = new ClientClass(groupOwnerAddress);
+                    clientClass.start();
 
                 }
             }
@@ -249,14 +267,63 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void run(){
                 try {
+
                     serverSocket = new ServerSocket(88888);
                     socket = serverSocket.accept();
+                    sendReceive = new SendReceive(socket);
+                    sendReceive.start();
+
                 } catch (IOException e){
                     e.printStackTrace();
                 }
             }
 
         }
+
+        private class SendReceive extends Thread{
+            // send receive method and variable
+            private Socket socket;
+            private InputStream inputStream;
+            private OutputStream outputStream;
+
+            public SendReceive(Socket skt){
+
+                socket = skt;
+                try {
+                    inputStream = socket.getInputStream();
+                    outputStream = socket.getOutputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void run(){
+                byte[] buffer = new byte[1024];
+                int bytes;
+
+                while(socket != null){
+                    try {
+                        bytes = inputStream.read(buffer);
+                        if (bytes > 0){
+                            handler.obtainMessage(MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            public void write(byte[] bytes){
+                try {
+                    outputStream.write(bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
 
         public class ClientClass extends Thread {
 
@@ -273,7 +340,11 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void run(){
                 try {
+
                     socket.connect(new InetSocketAddress(hostAdd, 8888),500);
+                    sendReceive = new SendReceive(socket);
+                    sendReceive.start();
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
